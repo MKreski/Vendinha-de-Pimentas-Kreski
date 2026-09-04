6f5573d4c5b3de72041cd4f182edcafd44bd77a1
MKreski <matheuskreski@gmail.com>
2026-09-04 19:47:31 -0300

    desenvolvimento inicial adicionando Cliente e Menu simples

---
+++ VendinhaKreski/Models/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendinhaKreski.Services.Validators;
+
+namespace VendinhaKreski.Models
+{
+    public class Cliente
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required, ValidaCpf(ErrorMessage = "CPF inválido."), StringLength(11)]
+        public string Cpf { get; set; }
+        [Required, DataType(DataType.Date)]
+        public DateOnly DataNascimento { get; set; }
+        [Required, MaxLength(100)]
+        public string Nome { get; set; }
+        [Required, EmailAddress(ErrorMessage = "Email inválido.")]
+        public string Email { get; set; }
+        public int Idade { get; set => CalcularIdade(); }
+
+        private void CalcularIdade()
+        {
+            var hoje = DateOnly.FromDateTime(DateTime.Today);
+            Idade = hoje.Year - DataNascimento.Year;
+            if (hoje < DataNascimento.AddYears(Idade))
+            {
+                Idade--;
+            }
+        }
+    }
+}
---
+++ VendinhaKreski/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Menu de teste");
+
+bool menu = true;
+while (menu)
+{
+    Console.WriteLine("=====================================");
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine("[1] - Adicionar Cliente");
+    Console.WriteLine("[2] - Listar Clientes");
+    Console.WriteLine("[3] - Editar Cliente");
+    Console.WriteLine("[4] - Deletar Cliente");
+    Console.WriteLine("[0] - Sair");
+    var opcao = 0;
+
+    try
+    {
+        bool opc = int.TryParse(Console.ReadLine(), out opcao);
+        if (opc)
+        {
+            Console.WriteLine($"Opção escolhida: {opcao}");
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Adicionar Cliente");
+                    break;
+                case 2:
+                    Console.WriteLine("Listar Clientes");
+                    break;
+                case 3:
+                    Console.WriteLine("Editar Cliente");
+                    break;
+                case 4:
+                    Console.WriteLine("Deletar Cliente");
+                    break;
+                case 0:
+                    Console.WriteLine("Saindo...");
+                    menu = false;
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("\n-=Opção inválida=-\n");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro: {ex.Message}");
+        menu = false;
+    }
+}
